5aeee6ec43fc343e130863883edd3a2dd0f2765b
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-06-16 10:27:46 -0700

    js and html updates.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,47 @@
-// function numOutput(num) {
-//   let numList = [];
-//   for (let i = 0; i <= num; i += 1) {
-//     numList.push(i);
-//     let stringNum = i.toString();
-//       if (stringNum.includes("3")) {
-//         numList.push("Won't you be my neighbor?");
-//       } else if (stringNum.includes("2")) {
-//         numList.push("Boop!");
-//       } else if (stringNum.includes("1")) {
-//         numList.push("Beep!");
-//       } else {
-//         numList.push(stringNum);
-//       }
-//       console.log(numList)
-//   }
-//   return numList;
-// }
-
-
 function numOutput(num) {
   let numWords = [];
   for (let i = 0; i <= num; i += 1) {
-    console.log(i)
-    let wordNum=i.toString();
+    numWords.push(i);
+    let wordNum = i.toString();
       if (wordNum.includes("3")) {
-        numWords.push(" " + "Won't you be my neighbor?");
+        numWords.push("Won't you be my neighbor?");
       } else if (wordNum.includes("2")) {
-        numWords.push(" " + "Boop!");
+        numWords.push("Boop!");
       } else if (wordNum.includes("1")) {
-        numWords.push(" " + "Beep!");
+        numWords.push("Beep!");
       } else {
-        numWords.push(" ")
+        numWords.push(wordNum);
       }
+      console.log(numWords)
   }
   return numWords;
 }
 
 
+// function numOutput(num) {
+//   let numWords = [];
+//   for (let i = 0; i <= num; i += 1) {
+//     console.log(i)
+//     let wordNum=i.toString();
+//       if (wordNum.includes("3")) {
+//         numWords.push(" " + "Won't you be my neighbor?");
+//       } else if (wordNum.includes("2")) {
+//         numWords.push(" " + "Boop!");
+//       } else if (wordNum.includes("1")) {
+//         numWords.push(" " + "Beep!");
+//       } else {
+//         numWords.push(" ")
+//       }
+//   }
+//   return numWords;
+// }
+
+
 $(document).ready(function() {
   $("#numList").submit(function (event) {
-    
+    event.preventDefault();
+    const userInput = parseInt($("#num").val());
+    const output = numOutput(userInput);
+    $("#formSubmit").text(output); 
   });
 });  
